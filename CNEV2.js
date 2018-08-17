b4cb1a2c80c1aca720307ecc72d84d079ae15921
angelav011301 <42008429+angelav011301@users.noreply.github.com>
2018-08-17 14:52:59 -0400

    CNE Planner V2

---
+++ CNEV2.js
@@ -0,0 +1,256 @@
+var eventArray= [];
+function results2() {
+  var date = "Date: " + document.getElementById('date').value;
+  var eventName = "Activity Name: " + document.getElementById('eventName').value;
+  var description = "Description: " + document.getElementById('description').value;
+  var event = {dateField : date, eventField :eventName, eventDescription: description};
+  var arrayLength = eventArray.length;
+  if(arrayLength == 0){
+    eventArray = [event];
+  }else{
+    eventArray[arrayLength] = event;
+  }
+  var results = "";
+
+  for(var i=0; i <eventArray.length;i++){
+    results += eventArray[i].dateField + "<br>" + eventArray[i].eventField + "<br>" + eventArray[i].eventDescription + "<br><br>";
+  }
+
+  document.getElementById("entry").innerHTML = results;
+  document.getElementById('date').value='';
+  document.getElementById('eventName').value='';
+  document.getElementById('description').value='';
+}
+var tripArray=[];
+function results1() {
+  var tripName = "Trip Name: " + document.getElementById('tripName').value;
+  var tripDate = "Trip Date: " + document.getElementById('tripDate').value;
+  var location = "Location: " + document.getElementById('location').value;
+  var departure = "Time of Departure: " + document.getElementById('departure').value;
+  var arrival = "Time of Arrival: " + document.getElementById('arrival').value;
+  var trip = {tripNField : tripName, tripDField :tripDate, locationField: location, departureField: departure, arrivalField: arrival};
+  var arrayLength = tripArray.length;
+  if(arrayLength == 0){
+    tripArray = [trip];
+  }else{
+    tripArray[arrayLength] = trip;
+  }
+  var results = "";
+
+  for(var i=0; i <tripArray.length;i++){
+    results += tripArray[i].tripNField + "<br>" + tripArray[i].tripDField + "<br>" + tripArray[i].locationField + "<br>" + tripArray[i].departureField + "<br>" + tripArray[i].arrivalField+ "<br><br>";
+  }
+
+  document.getElementById("tripEntry").innerHTML = results;
+  document.getElementById('tripName').value='';
+  document.getElementById('tripDate').value='';
+  document.getElementById('location').value='';
+  document.getElementById('departure').value='';
+  document.getElementById('arrival').value='';
+}
+
+
+// CHECK LIST //
+
+var taskInput=document.getElementById("new-task");//Add a new task.
+var addButton=document.getElementsByTagName("button")[0];//first button
+var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
+var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+
+
+//New task list item
+var createNewTaskElement=function(taskString){
+
+	var listItem=document.createElement("li");
+
+	//input (checkbox)
+	var checkBox=document.createElement("input");//checkbx
+	//label
+	var label=document.createElement("label");//label
+	//input (text)
+	var editInput=document.createElement("input");//text
+	//button.edit
+	var editButton=document.createElement("button");//edit button
+
+	//button.delete
+	var deleteButton=document.createElement("button");//delete button
+
+	label.innerText=taskString;
+
+	//Each elements, needs appending
+	checkBox.type="checkbox";
+	editInput.type="text";
+
+	editButton.innerText="Edit";//innerText encodes special characters, HTML does not.
+	editButton.className="edit";
+	deleteButton.innerText="Delete";
+	deleteButton.className="delete";
+
+
+
+	//and appending.
+	listItem.appendChild(checkBox);
+	listItem.appendChild(label);
+	listItem.appendChild(editInput);
+	listItem.appendChild(editButton);
+	listItem.appendChild(deleteButton);
+	return listItem;
+}
+
+
+
+var addTask=function(){
+	console.log("Add Task...");
+	//Create a new list item with the text from the #new-task:
+	var listItem=createNewTaskElement(taskInput.value);
+
+	//Append listItem to incompleteTaskHolder
+	incompleteTaskHolder.appendChild(listItem);
+	bindTaskEvents(listItem, taskCompleted);
+
+	taskInput.value="";
+
+}
+
+//Edit an existing task.
+
+var editTask=function(){
+console.log("Edit Task...");
+console.log("Change 'edit' to 'save'");
+
+
+var listItem=this.parentNode;
+
+var editInput=listItem.querySelector('input[type=text]');
+var label=listItem.querySelector("label");
+var containsClass=listItem.classList.contains("editMode");
+		//If class of the parent is .editmode
+		if(containsClass){
+
+		//switch to .editmode
+		//label becomes the inputs value.
+			label.innerText=editInput.value;
+		}else{
+			editInput.value=label.innerText;
+		}
+
+		//toggle .editmode on the parent.
+		listItem.classList.toggle("editMode");
+}
+
+
+
+
+//Delete task.
+var deleteTask=function(){
+		console.log("Delete Task...");
+
+		var listItem=this.parentNode;
+		var ul=listItem.parentNode;
+		//Remove the parent list item from the ul.
+		ul.removeChild(listItem);
+
+}
+
+
+//Mark task completed
+var taskCompleted=function(){
+		console.log("Complete Task...");
+
+	//Append the task list item to the #completed-tasks
+	var listItem=this.parentNode;
+	completedTasksHolder.appendChild(listItem);
+				bindTaskEvents(listItem, taskIncomplete);
+
+}
+
+
+var taskIncomplete=function(){
+		console.log("Incomplete Task...");
+//Mark task as incomplete.
+	//When the checkbox is unchecked
+		//Append the task list item to the #incomplete-tasks.
+		var listItem=this.parentNode;
+	incompleteTaskHolder.appendChild(listItem);
+			bindTaskEvents(listItem,taskCompleted);
+}
+
+
+
+var ajaxRequest=function(){
+	console.log("AJAX Request");
+}
+
+//The glue to hold it all together.
+
+
+//Set the click handler to the addTask function.
+addButton.onclick=addTask;
+addButton.addEventListener("click",addTask);
+addButton.addEventListener("click",ajaxRequest);
+
+
+var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
+	console.log("bind list item events");
+//select ListItems children
+	var checkBox=taskListItem.querySelector("input[type=checkbox]");
+	var editButton=taskListItem.querySelector("button.edit");
+	var deleteButton=taskListItem.querySelector("button.delete");
+
+
+			//Bind editTask to edit button.
+			editButton.onclick=editTask;
+			//Bind deleteTask to delete button.
+			deleteButton.onclick=deleteTask;
+			//Bind taskCompleted to checkBoxEventHandler.
+			checkBox.onchange=checkBoxEventHandler;
+}
+
+//cycle over incompleteTaskHolder ul list items
+	//for each list item
+	for (var i=0; i<incompleteTaskHolder.children.length;i++){
+
+		//bind events to list items chldren(tasksCompleted)
+		bindTaskEvents(incompleteTaskHolder.children[i],taskCompleted);
+	}
+
+
+//cycle over completedTasksHolder ul list items
+	for (var i=0; i<completedTasksHolder.children.length;i++){
+	//bind events to list items chldren(tasksIncompleted)
+		bindTaskEvents(completedTasksHolder.children[i],taskIncomplete);
+	}
+
+
+
+  // Planner//
+  function PlannerStuff() {
+    console.log("works");
+    var x = document.getElementById("myPlanner");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+  // Agenda//
+  function Agenda() {
+    var x = document.getElementById("myAgenda");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+// Agenda//
+function clist() {
+  var x = document.getElementById("myCL");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
